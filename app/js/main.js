dc03c252375fb242a0fc28a8e40c8c920777660e
serg5serg <serduksergey2@gmail.com>
2020-05-29 01:19:55 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
+  $(".slider_user-rating").rateYo({
+    rating: 3.2,
+    readOnly: true,
+  });
+  $(".slider_conteiner").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+  });
+
   $("#Container").mixItUp({});
 });
--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,8 @@ gulp.task("style", function () {
     .src([
       "node_modules/normalize.css/normalize.css",
       "node_modules/slick-carousel/slick/slick.css",
-      "node_modules/magnific-popup/dist/magnific-popup.css",
+      // "node_modules/magnific-popup/dist/magnific-popup.css",
+      // "node_mobules/rateyo/src/jquery.rateyo.min.css",
     ])
     .pipe(concat("libs.min.css"))
     .pipe(cssmin())
@@ -39,6 +40,7 @@ gulp.task("script", function () {
       "node_modules/slick-carousel/slick/slick.js",
       "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
       // "node_mobules/mixitup/dist/mixitup.js",
+      // "node_mobules/rateyo/src/jquery.rateyo.min.js",
     ])
     .pipe(concat("libs.min.js"))
     .pipe(uglify())
